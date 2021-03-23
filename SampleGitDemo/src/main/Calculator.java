1fca127fe9c4f539656814fddb57b9fa3c216a17
mananjain10 <mananfootball@gmail.com>
2021-03-23 10:54:43 +0530

    added masterfun1

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -35,5 +35,10 @@ public class Calculator {
 	      System.out.println(num1*num1*num1);
 
 	  }
+	
+	public static void masterfun1(int num1){
+	      System.out.println(num1*num1*num1);
+
+	  }
 
 }
